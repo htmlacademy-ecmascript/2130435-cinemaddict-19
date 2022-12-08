ca7e3af9e9377a2a150f66d9d9a2f87c0d6498ff
Andy <roshitu@yandex.ru>
2022-12-08 09:34:20 +0300

    Правки Eslint

--- src/main.js
+++ src/main.js
@@ -1,24 +1,7 @@
-import { COMMENT, DANCE_LIFE, GREAT_FLAMARION, POPEYE_THE_SAILOR, POPUP_FILM, SAGEBRUSH_TRAIL, THE_MAN } from './mock.js';
 import FooterPresenter from './presenter/footer-presenter.js';
 import HeaderPresenter from './presenter/header-presenter.js';
 import MainPagePresenter from './presenter/main-page-presenter.js';
 import PopupPresenter from './presenter/popup-presenter.js';
-import { render } from './render.js';
-import NewFilmPopupComment from './view/atom/film-popup-comment.js';
-import NewPopupFilmDetailsInfoView from './view/atom/film-popup-info.js';
-import NewFooterStatisticsView from './view/atom/footer-statistics.js';
-import NewHeaderProfileView from './view/atom/header-profile.js';
-import NewFilmPopupCommentsList from './view/molecule/film-popup-comments-list.js';
-import NewPopupFilmControlsView from './view/molecule/film-popup-controls.js';
-import NewFilterMenuView from './view/molecule/main-navigate.js';
-import NewSortListView from './view/molecule/sort-list.js';
-import NewFilmCardView from './view/organism/film-card.js';
-import NewFilmSection from './view/page/film-section.js';
-import NewFilmListView from './view/template/film-list.js';
-import NewFilmPopupView from './view/template/film-popup.js';
-import NewFilmPopupBottomContainer from './view/wrapper/film-popup-bottom-container.js';
-import NewFilmPopupTopContainer from './view/wrapper/film-popup-top-container.js';
-
 
 const siteBodyElement = document.body;
 const siteMainElement = document.querySelector('.main');
