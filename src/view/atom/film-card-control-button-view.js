39d1fd9d171320c26a9f7c4f1a9fb7153cf6d5d6
Andrey Shi <andy.roshitu@gmail.com>
2022-12-16 14:53:59 +0300

    Исправление сеттера

--- src/view/atom/film-card-control-button-view.js
+++ src/view/atom/film-card-control-button-view.js
@@ -37,7 +37,7 @@ export default class NewFilmCardControlButtonView {
     return this._active;
   }
 
-  set active(newState = this._active) {
+  set active(newState) {
     this._active = newState;
   }
 
