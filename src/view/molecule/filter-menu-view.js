51500a557d8e9b9f9d89f156629f989168aed182
Andy Rosa <andy.roshitu@gmail.com>
2023-01-08 17:38:23 +0300

    Добавляет счетчик фильтрации фильмам

--- src/view/molecule/filter-menu-view.js
+++ src/view/molecule/filter-menu-view.js
@@ -2,12 +2,12 @@ import { createElement, render } from '../../framework/render.js';
 import NewMainNavigateItemView from '../atom/main-navigation-item-view.js';
 import AbstractView from '../../framework/view/abstract-view.js';
 
-const NAVIGATE_ITEMS = [
-  new NewMainNavigateItemView('All movies', '', true),
-  new NewMainNavigateItemView('Watchlist', '13'),
-  new NewMainNavigateItemView('History', '4'),
-  new NewMainNavigateItemView('Favorites', '8'),
-];
+const NAVIGATE_ITEMS_NAMES = {
+  ALL: 'All movies',
+  WATCH: 'Watchlist',
+  HISTORY: 'History',
+  FAVORITE: 'Favorites'
+};
 
 function createMainNavigate() {
   return '<nav class="main-navigation"></nav>';
@@ -15,15 +15,39 @@ function createMainNavigate() {
 
 export default class NewFilterMenuView extends AbstractView {
   #element = null;
-  #buttons;
+  #films;
+  #watchlistCounter = 0;
+  #historyCounter = 0;
+  #favoritesCounter = 0;
 
-  constructor(...buttons) {
+  constructor(filmsModel) {
     super();
-    if (!buttons.length) {
-      this.#buttons = NAVIGATE_ITEMS;
-    } else {
-      this.#buttons = [...buttons];
-    }
+    this.#films = filmsModel.films;
+  }
+
+  #calculateCorrectFilmsMarks() {
+    this.#films.forEach(({user_details: userDetails }) => {
+      const { watchlist, already_watched: history, favorite } = userDetails;
+      if (watchlist) {
+        this.#watchlistCounter++;
+      }
+      if (history) {
+        this.#historyCounter++;
+      }
+      if (favorite) {
+        this.#favoritesCounter++;
+      }
+    });
+  }
+
+  #createNavigateItems() {
+    this.#calculateCorrectFilmsMarks();
+    return [
+      new NewMainNavigateItemView(NAVIGATE_ITEMS_NAMES.ALL, '', true),
+      new NewMainNavigateItemView(NAVIGATE_ITEMS_NAMES.WATCH, this.#watchlistCounter),
+      new NewMainNavigateItemView(NAVIGATE_ITEMS_NAMES.HISTORY,this.#historyCounter),
+      new NewMainNavigateItemView(NAVIGATE_ITEMS_NAMES.FAVORITE,this.#favoritesCounter)
+    ];
   }
 
   get template() {
@@ -33,7 +57,8 @@ export default class NewFilterMenuView extends AbstractView {
   get element() {
     if (!this.#element) {
       this.#element = createElement(this.template);
-      this.#buttons.forEach((button) => render(button, this.#element));
+      const buttons = this.#createNavigateItems();
+      buttons.forEach((button) => render(button, this.#element));
     }
     return this.#element;
   }
