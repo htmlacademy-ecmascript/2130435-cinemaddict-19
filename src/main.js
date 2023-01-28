556f46c7dc65b9e2f9b95d740d25dea884c39169
Andy Rosa <andy.roshitu@gmail.com>
2023-01-28 20:38:33 +0300

    fix: lint. WIP

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import CommentsModel from './model/comments-model.js';
 import FilmsModel from './model/films-model.js';
 import AppPresenter from './presenter/app-presenter.js';
 
-const header = document.querySelector('.header');
-const main = document.querySelector('.main');
-const footer = document.querySelector('.footer');
+// const header = document.querySelector('.header');
+// const main = document.querySelector('.main');
+// const footer = document.querySelector('.footer');
 
 const commentsModel = new CommentsModel();
 const filmsModel = new FilmsModel();
